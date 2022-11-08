deb5a6f63dcbba20d2d186bfd980027bc8a84992
Gbrock0110 <62383743+Gbrock0110@users.noreply.github.com>
2022-11-08 19:41:23 -0300

    Add files via upload

---
+++ tapclick/Consultar Linhas/address.js
@@ -0,0 +1,16 @@
+function address() {
+  var input, filter, span, a, i, txtValue;
+  input = document.getElementById("explore-search");
+  filter = input.value.toUpperCase();
+  span = document.getElementById("line-detail");
+
+  for (i = 0; i < span.length; i++) {
+    a = span[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      span[i].style.display = "";
+    } else {
+      span[i].style.display = "none";
+    }
+  }
+}
